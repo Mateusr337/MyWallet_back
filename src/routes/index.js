bd52434821552bcb638e56f36763dcd0b8798e24
Lucas Cardoso Tavares <carvares.dev@gmail.com>
2022-02-15 10:07:18 -0300

    preparando review

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import operationRouter from './operationRouter.js';
-import authRouter from './authRouter.js';
-
-const router = Router();
-router.use(authRouter);
-router.use(operationRouter);
-
-export default router;
--- src/schemas/signUpSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const signUpSchema = joi.object({
-    name: joi.string().required(),
-    email: joi.string().email().required(),
-    password: joi.required()
-});
-
-export default signUpSchema;
